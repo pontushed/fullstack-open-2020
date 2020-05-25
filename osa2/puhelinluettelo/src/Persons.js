6ce3821f5716a8039106a203d1bda3fbded74720
Pontus <pontus.hedlund@gmail.com>
2020-05-25 14:42:27 +0300

    Osa 2 tehty.

--- osa2/puhelinluettelo/src/Persons.js
+++ osa2/puhelinluettelo/src/Persons.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
-const Persons = ({ persons, filterName }) => {
+const Persons = ({ persons, filterName, onDelete }) => {
   const listPersons = filterName.length
     ? persons.filter((p) => {
         return p.name.toLowerCase().includes(filterName)
       })
     : persons
+
+  const handleClick = (event) => {
+    onDelete(event.target.id)
+  }
+
   return (
     <ul>
       {listPersons.map((p) => (
-        <li key={p.name}>
+        <li key={p.id}>
           {p.name} {p.number}
+          <button id={p.id} onClick={handleClick}>
+            Remove
+          </button>
         </li>
       ))}
     </ul>
