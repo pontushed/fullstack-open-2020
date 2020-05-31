d2fccea7c21d655dc19658666df89e4607ba96bb
Pontus <pontus.hedlund@gmail.com>
2020-05-31 16:31:14 +0300

    Part 6 completed

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList from './components/AnecdoteList'
+import ConnectedAnecdoteForm from './components/AnecdoteForm'
+import ConnectedAnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
@@ -17,8 +17,8 @@ const App = () => {
       <h2>Anecdotes</h2>
       <Notification />
       <Filter />
-      <AnecdoteForm />
-      <AnecdoteList />
+      <ConnectedAnecdoteForm />
+      <ConnectedAnecdoteList />
     </div>
   )
 }
